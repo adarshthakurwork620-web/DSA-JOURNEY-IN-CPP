38c015df81b9afa4d746363d3ac870c0897f2540
Adarsh22 <adarshthakurwork620@gmail.com>
2026-08-01 19:56:49 +0530

    Array/Swap_Alternate.cpp

---
+++ Array/Swap_Alternate.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int input(int arr[] , int n)
+{
+    cout<<"Enter the elemant:";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+}
+int sawp_alternate(int arr[] , int n)
+{
+    for (int i = 0; i < n ; i=i+2)
+    {
+        if (i+1 < n)
+        {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        } 
+    }    
+}
+int display(int arr[] , int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }   
+}
+int main()
+{
+    int arr[100],n;
+    cout<<"Enter the size of array:";
+    cin>>n;
+    input(arr,n);
+    sawp_alternate( arr ,n);
+    display(arr, n);
+}
