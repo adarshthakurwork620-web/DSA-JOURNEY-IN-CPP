2144c55e61c8541f23ffded195dd9b4f4f138bd6
Adarsh22 <adarshthakurwork620@gmail.com>
2026-08-06 23:41:32 +0530

    Remove_Duplicates_in_sorted_array.cpp

--- Array/Remove_Duplicates_in_sorted_array.cpp
+++ Array/Remove_Duplicates_in_sorted_array.cpp
@@ -5,7 +5,7 @@ int remove_duplicates(int arr[],int size)
     int i = 0 ;
     for(int j = 1 ; j < size ;j++)
     {
-        if (arr[i] == arr[j])
+        if (arr[i] != arr[j])
         {
             arr[i+1]=arr[j];
             i++;
@@ -18,7 +18,7 @@ int remove_duplicates(int arr[],int size)
 }
 int main()
 {
-    int arr[8]={4,3,2,7,8,2,3,1};
+    int arr[8]={1,1,2,2,3,3,4,4};
     remove_duplicates(arr ,8);
 }
     
