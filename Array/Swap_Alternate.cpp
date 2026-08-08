47c3827039d1b4334908c0cb6d028482c7d53e35
Adarsh22 <adarshthakurwork620@gmail.com>
2026-08-08 19:35:18 +0530

    Swap_Alternate.cpp

--- Array/Swap_Alternate.cpp
+++ Array/Swap_Alternate.cpp
@@ -12,12 +12,14 @@ int sawp_alternate(int arr[] , int n)
 {
     for (int i = 0; i < n ; i=i+2)
     {
+        // if (i+1 < n)
+        // {
+        //     int temp = arr[i];
+        //     arr[i] = arr[i+1];
+        //     arr[i+1] = temp;
+        // } 
         if (i+1 < n)
-        {
-            int temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        } 
+        swap(arr[i],arr[i+1]);
     }    
 }
 int display(int arr[] , int n)
